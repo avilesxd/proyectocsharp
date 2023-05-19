aa5c13fedd25a0c1ad42eb1d1a607b842ec1ad32
Ignacio Avilés <88689569+avilesxd@users.noreply.github.com>
2023-05-19 08:54:37 -0400

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Database/database.sql
@@ -0,0 +1,41 @@
+﻿create database CompanyIHJ;
+
+use CompanyIHJ;
+
+CREATE TABLE Administrador (
+ID INT IDENTITY (1,1) PRIMARY KEY,
+Nombre VARCHAR (50) not null,
+Apellido VARCHAR (50) not null,
+Rut INT not null,
+Telefono INT,
+Correo VARCHAR (50)
+);
+
+
+CREATE TABLE Videojuegos(
+ID INT IDENTITY(1,1) PRIMARY KEY,
+Nombre VARCHAR (50) not null,
+Categoria VARCHAR (50) not null,
+Precio INT not null,
+Stock INT not null,
+);
+
+
+CREATE TABLE Clientes(
+ID INT IDENTITY (1,1) PRIMARY KEY,
+Nombre VARCHAR (50) not null,
+Apellido VARCHAR (50) not null,
+Rut INT not null,
+Telefono INT,
+Correo VARCHAR (50),
+Direccion VARCHAR (50),
+);
+
+CREATE TABLE Registros (
+ID INT IDENTITY (1,1) PRIMARY KEY,
+DetalleCompra VARCHAR (50) not null,
+ID_Cliente INT FOREIGN KEY REFERENCES Clientes(ID),
+ID_Videojuego INT FOREIGN KEY REFERENCES Videojuegos(ID),
+);
+
+SELECT * FROM Registros;
