98967e5bcd93049ee665e684103a9e3f31fdfb57
Ignacio Avilés <88689569+avilesxd@users.noreply.github.com>
2023-05-19 00:24:25 -0400

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Vista/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp1.Vista;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtuser_Enter(object sender, EventArgs e)
+        {
+            if (txtuser.Text == "USUARIO")
+            {
+                txtuser.Text = "";
+                txtuser.ForeColor = Color.LightGray;
+            }
+        }
+
+        private void txtuser_Leave(object sender, EventArgs e)
+        {
+            if(txtuser.Text == "")
+            {
+                txtuser.Text = "USUARIO";
+                txtuser.ForeColor = Color.DimGray;
+            }
+        }
+
+        private void txtpass_Enter(object sender, EventArgs e)
+        {
+            if(txtpass.Text == "CONTRASEÑA")
+            {
+                txtpass.Text = "";
+                txtpass.ForeColor = Color.LightGray;
+                txtpass.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void txtpass_Leave(object sender, EventArgs e)
+        {
+            if( txtpass.Text == "")
+            {
+                txtpass.Text = "CONTRASEÑA";
+                txtpass.ForeColor = Color.DimGray;
+                txtpass.UseSystemPasswordChar = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Registro registro = new Registro();
+            registro.Show();
+        }
+
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+            string usuarioPrueba = "titoconmoster";
+            string passwordPrueba = "titosinmoster";
+            string usuarioLogin = txtuser.Text;
+            string passwordLogin = txtpass.Text;
+
+            
+            if (usuarioPrueba.Equals(usuarioLogin) || passwordLogin.Equals(passwordPrueba))
+            {
+                MessageBox.Show($"Bienvendio {usuarioLogin}");
+            }
+            else {
+                MessageBox.Show("tito peruano los datos no coinciden");
+            }
+        }
+    }
+}
